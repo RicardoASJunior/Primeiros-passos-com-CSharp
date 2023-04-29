08fc52c50e0a731ba797553caa660c3badc5e69e
Ricardo <ric.asjunior@gmail.com>
2023-04-29 20:32:59 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+class Programa{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Word!!!");
+
+        Console.WriteLine("Pressione Enter para avançar...");
+        Console.ReadLine();
+
+        //------------------------------------------
+
+        Console.WriteLine("Projeto 2 - Criando Variáveis");
+
+        int idade;
+        idade = 22;
+        Console.WriteLine("Minha idade é: " + idade);
+        idade = 27 -5;
+        Console.WriteLine(idade);
+        idade = 5 * 2 -6;
+
+        Console.WriteLine(idade);
+        Console.WriteLine("Pressione Enter para avançar...");
+        Console.ReadLine();
+
+        //---------------------------------------------
+
+        Console.WriteLine("Projeto 3 - Variáveis de ponto Flutuante");
+
+        double salario;
+        salario = 2200.50;
+
+        Console.WriteLine(salario);
+
+        double divisao;
+        divisao = 7.0 / 5;
+
+        Console.WriteLine("O resultado é: " + divisao);
+        Console.WriteLine("Pressione Enter para avançar...");
+        Console.ReadLine();
+
+        //-----------------------------------------------
+
+        Console.WriteLine("Projeto 4 - Outras variáveis numéricas");
+
+        long x = 200000000000000000;
+        Console.WriteLine(x);
+
+        short y = 15000;
+        Console.WriteLine(y);
+
+        float altura = 1.78f;
+        Console.WriteLine(altura);
+
+        Console.WriteLine("Pressione Enter para avançar...");
+        Console.ReadLine();
+
+        //----------------------------------------------
+
+        Console.WriteLine("Projeto 5 - Caracteres e Textos.");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65; // Transformando numero em letra.. consultar tabela char
+        Console.WriteLine(letra);
+
+
+        letra = (char)(65+1); // Transformando numero em letra.. consultar tabela char
+        Console.WriteLine(letra);
+
+        Console.WriteLine("Pressione Enter para avançar...");
+        Console.ReadLine();
+
+        string frase = "Encapsulando uma frase";
+        Console.WriteLine(frase);
+        
+
+    }
+}
